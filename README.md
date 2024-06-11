0b0ecd24ad4b233777a464ed24319d7d82896219
Ronnie Jakobsgaard <ronnie192@gmail.com>
2024-06-11 23:47:46 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # marine-telemetry
+
+A Marine Telemetry service that provides a RESTful API for managing telemetry data from marine vessels.
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes.
---
+++ src/main/java/org/nauticasensus/telemetry/telemetry_service/TelemetryServiceApplication.java
@@ -0,0 +1,12 @@
+package org.nauticasensus.telemetry.telemetry_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelemetryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelemetryServiceApplication.class, args);
+	}
+}
