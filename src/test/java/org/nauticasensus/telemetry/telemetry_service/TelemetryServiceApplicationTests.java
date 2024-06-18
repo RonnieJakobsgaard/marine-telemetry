0d9bdbd4d7c5147d35d7b95e7cb95168805f2513
Ronnie Jakobsgaard <ronnie192@gmail.com>
2024-06-18 21:10:42 +0200

    Cleanup unused imports

--- src/test/java/org/nauticasensus/telemetry/telemetry_service/TelemetryServiceApplicationTests.java
+++ src/test/java/org/nauticasensus/telemetry/telemetry_service/TelemetryServiceApplicationTests.java
@@ -2,10 +2,7 @@ package org.nauticasensus.telemetry.telemetry_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
-import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.utility.DockerImageName;
 
 @Import(TestContainersConfiguration.class)
 @SpringBootTest
